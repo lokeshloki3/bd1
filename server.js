1c288ab64ed70640b00035ed21048b212d0f9a62
lokeshloki3 <lokesh1992p@gmail.com>
2024-12-23 14:35:04 +0530

    post request

--- server.js
+++ server.js
@@ -8,3 +8,10 @@ app.listen(3000, () =>{
 app.get("/", (request,response)=>{
     response.send("Hello");
 })
+
+app.post("/cars/brand", (request, response) =>{
+    const {name, brand} = request.body;
+    console.log(name);
+    console.log(brand);
+    response.send("Cars submitted successfully");
+})
